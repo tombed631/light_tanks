ed074bdf212553e6376b6663412ca126ec9f37cf
Marcin Adrian (marcadr342) <marcadr342@student.polsl.pl3>
2016-03-25 21:58:54 +0100

    Add menu options + selection with the mouse

--- GKProject/Includes.h
+++ GKProject/Includes.h
@@ -2,6 +2,9 @@
 #define Includes_hpp
 #endif
 
+#define NOMINMAX
+
+
 #include <Windows.h>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Graphics.hpp>
@@ -10,6 +13,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstdlib>
+#include <limits>
+
 
 using namespace sf;
 using namespace std;
