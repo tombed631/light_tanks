5e20540c5fd6fcd7ebfe53fd6bc938dbc0d07b8b
Tom Bed <tombed631@gmail.com>
2016-06-11 14:12:11 +0200

    Change comments !!!

--- GKProject/Collision.h
+++ GKProject/Collision.h
@@ -8,7 +8,7 @@ class BoundingBox
 	{
 	public:
 		template <class T>
-		BoundingBox(const T& object) // Calculate the four points of the OBB from a transformed (scaled, rotated...) sprite
+		BoundingBox(const T& object) 
 		{
 			Transform trans = object.getTransform();
 			IntRect local = object.getTextureRect();
@@ -17,9 +17,7 @@ class BoundingBox
 			points[2] = trans.transformPoint((float)local.width, (float)local.height);
 			points[3] = trans.transformPoint(0.f, (float)local.height);
 		}
-
 		sf::Vector2f points[4];
-
 		void transformateToaxis(const sf::Vector2f& axis, float& min, float& max)
 		{
 			min = (points[0].x*axis.x + points[0].y*axis.y);
@@ -41,7 +39,6 @@ public:
 		bool isCollision(const Sprite& object1, const T& object2) {
 			BoundingBox OBB1(object1);
 			BoundingBox OBB2(object2);
-			// Create the four distinct axes that are perpendicular to the edges of the two rectangles
 			sf::Vector2f axes[4] = {
 				Vector2f(OBB1.points[1].x - OBB1.points[0].x,
 				OBB1.points[1].y - OBB1.points[0].y),
@@ -53,16 +50,11 @@ public:
 				OBB2.points[0].y - OBB2.points[1].y)
 			};
 
-			for (int i = 0; i<4; i++) // For each axis...
+			for (int i = 0; i<4; i++)
 			{
 				float minOBB1, maxOBB1, minOBB2, maxOBB2;
-
-				// ... project the points of both OBBs onto the axis ...
 				OBB1.transformateToaxis(axes[i], minOBB1, maxOBB1);
 				OBB2.transformateToaxis(axes[i], minOBB2, maxOBB2);
-
-				// ... and check whether the outermost projected points of both OBBs overlap.
-				// If this is not the case, the Seperating axis Theorem states that there can be no collision between the rectangles
 				if (!((minOBB2 <= maxOBB1) && (maxOBB2 >= minOBB1)))
 					return false;
 			}
